655af1d322f3f613ee0c7cef91174355158073a2
ETTAHAIRY-mohamed <mohamed.ettahairy@usmba.ac.ma>
2024-08-06 01:05:56 +0100

    v1

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    # Enregistrement des blueprints
+    from .views import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/schemas.py
@@ -0,0 +1,10 @@
+from . import ma
+from .models import User
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
