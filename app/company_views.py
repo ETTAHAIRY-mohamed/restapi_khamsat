b5a35b92641aae86d362905635f0365795dcab81
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2024-08-09 23:44:00 +0100

    add search query for companies with auth required

--- app/company_views.py
+++ app/company_views.py
@@ -3,14 +3,24 @@ from flask.views import MethodView
 from app.extensions import db
 from app.models import Company
 from app.schemas import CompanySchema
+from flask import request
+from flask_jwt_extended import jwt_required
 
 blp = Blueprint('companies', __name__, url_prefix='/companies', description='Operations on companies')
 
 @blp.route('/')
 class Companies(MethodView):
+    @jwt_required()
     @blp.response(200, CompanySchema(many=True))
     def get(self):
-        return Company.query.all()
+        # Get query parameters for search
+        search_query = request.args.get('search')
+        query = Company.query
+        # Apply search filtering
+        if search_query:
+            query = query.filter(Company.name.ilike(f'%{search_query}%'))
+
+        return query.all()
 
     @blp.arguments(CompanySchema)
     @blp.response(201, CompanySchema)
--- app/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, jsonify
-from flask.views import MethodView
-from .models import User
-from .schemas import user_schema, users_schema
-from . import db
-
-main = Blueprint('main', __name__)
